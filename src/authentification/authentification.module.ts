8432b5d66b768d40b67674576ea71141a04eb9cc
Adnan <adnan.goulamaly@gmail.com>
2020-11-26 12:41:21 +0400

    fonctioonnalité qui permet de généré un jwt (50%)

--- src/authentification/authentification.module.ts
+++ src/authentification/authentification.module.ts
@@ -1,9 +1,16 @@
 import { Module } from "@nestjs/common";
 import {AuthentificationController} from "./authentification.controller";
 import {AuthentificationService} from "./authentification.service";
-import {UserService} from "../user/user.services"
+import {UserService} from "../user/user.services";
+import { JwtService } from "@nestjs/jwt";
+//import {JwtModule} from "@nestjs/jwt";
 
 @Module({
+ /* imports:[
+    JwtModule.registerAsync({
+
+    })
+  ], */
   controllers: [AuthentificationController],
   providers: [AuthentificationService,UserService],
 })
--- src/authentification/authentification.service.ts
+++ src/authentification/authentification.service.ts
@@ -2,13 +2,21 @@ import { Injectable } from '@nestjs/common';
 import { from } from 'rxjs';
 import {getConnection} from "typeorm";
 import {Utilisateur} from "../models/utilisateur.entity"
+//import {JwtService} from "@nestjs/jwt"
 
 const bcrypt = require('bcrypt');
 
 
 @Injectable()
 export class AuthentificationService {
-  
+
+    constructor(){}
+
+    // payload correspond aux informations qui seront stockées dans le jwt. ex. les infos d'un User
+    generateJwt(payload:Object){
+      
+    }
+
     async hashPassword(password:string){
       const salt = await bcrypt.genSalt(10)
         const hashPassword = await bcrypt.hash(password, salt);
