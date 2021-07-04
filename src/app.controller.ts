eee4a5afeb3814489918fe494052dd4122a75320
Adnan <adnan.goulamaly@gmail.com>
2021-07-04 21:17:40 +0400

    rectification du app controlle rpour heroku

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,7 +3,7 @@ import { ApiTags } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
 @ApiTags("app module")
-@Controller()
+@Controller("/")
 export class AppController {
   constructor(private readonly appService: AppService) { }
 
